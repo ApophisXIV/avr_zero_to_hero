fb9c20bf3e3fc742b60955911fa67a0bf8fb6c5b
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2025-04-20 03:03:04 -0300

    [ADD] implement clock prescaler test

--- 00_clock_and_blink/main.c
+++ 00_clock_and_blink/main.c
@@ -2,13 +2,33 @@
 #include "board.h"
 
 #include <avr/io.h>
-#include <util/delay.h>
+
+#include "Drivers/clock/clock.h"
+
+void delay_nop(uint16_t ms) {
+    while (ms--) {
+        for (uint16_t i = 0; i < 800; i++) {
+            __asm__ __volatile__("nop");
+        }
+    }
+}
 
 int main(void) {
+
     DDRB |= (1 << PB5);    // Set pin 13 (PB5) as output
+
     while (1) {
-        PORTB ^= (1 << PB5);    // Toggle pin 13 (PB5)
-        _delay_ms(400);
+
+#ifdef USE_CPU_CLOCK_PRESCALER_AT_RUNTIME
+        for (clk_prescaler_t prescaler = CLK_DIV_1; prescaler <= CLK_DIV_256; prescaler++) {
+            clock_prescaler_config(prescaler);
+            PORTB ^= (1 << PB5);
+            delay_nop(200);
+        }
+#else
+        PORTB ^= (1 << PB5);
+        delay_nop(200);
+#endif
     }
 
     return 0;
