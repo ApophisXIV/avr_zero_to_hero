e7619f7d261fad720b8e411a2aa49c09a8a7afb0
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2025-04-27 19:40:21 -0300

    [ADD] Implement UART driver with initialization and byte read/write functions. TODO: Selectable dataframe, sync mode, IT bases UART

---
+++ 03_uart_driver/Drivers/uart/uart.h
@@ -0,0 +1,24 @@
+/**
+ * @file uart.h
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief
+ * @version 0.1
+ * @date 2025-04-26
+ *
+ * @copyright Copyright (c) 2025. All rights reserved.
+ *
+ * Licensed under the MIT License, see LICENSE for details.
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef UART_H
+#define UART_H
+
+#include <stdio.h>
+
+#define BAUD 9600
+
+void UART_init(void);
+
+#endif    // UART_H
