a68d2e834ee9da095a08d7952cac199f283b9bfb
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2025-05-12 06:29:20 -0300

    [FIX] Update UART function names to follow consistent naming convention

--- 03_uart_driver/Drivers/uart/uart.c
+++ 03_uart_driver/Drivers/uart/uart.c
@@ -5,11 +5,11 @@
 #include <avr/io.h>
 #include <util/setbaud.h>
 
-static int uart0_write_byte(int write_byte, FILE *stream);
-static int uart0_read_byte(FILE *stream);
+static int UART_write_byte(int write_byte, FILE *stream);
+static int UART_read_byte(FILE *stream);
 
-static FILE uart0_stdin  = FDEV_SETUP_STREAM(NULL, uart0_read_byte, _FDEV_SETUP_READ);
-static FILE uart0_stdout = FDEV_SETUP_STREAM(uart0_write_byte, NULL, _FDEV_SETUP_WRITE);
+static FILE UART_stdin  = FDEV_SETUP_STREAM(NULL, UART_read_byte, _FDEV_SETUP_READ);
+static FILE UART_stdout = FDEV_SETUP_STREAM(UART_write_byte, NULL, _FDEV_SETUP_WRITE);
 
 void UART_init(void) {
     UBRR0H = UBRRH_VALUE;    // Mirar setbaud.h
@@ -22,8 +22,8 @@ void UART_init(void) {
     UCSR0B = (1 << TXEN0) | (1 << RXEN0);      // Enable RX and TX
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);    // 8 data bits, no parity, 1 stop bit
 
-    stdin  = &uart0_stdin;
-    stdout = &uart0_stdout;
+    stdin  = &UART_stdin;
+    stdout = &UART_stdout;
 }
 
 // void UART_IT_init(void) {
@@ -36,13 +36,13 @@ bool UART_is_available(void) {
     return UCSR0A & (1 << RXC0);
 }
 
-static int uart0_write_byte(int write_byte, FILE *stream) {
+static int UART_write_byte(int write_byte, FILE *stream) {
     loop_until_bit_is_set(UCSR0A, UDRE0);    // Wait for empty transmit buffer
     UDR0 = write_byte;                       // write one byte to UART0
     return 0;
 }
 
-static int uart0_read_byte(FILE *stream) {
+static int UART_read_byte(FILE *stream) {
     loop_until_bit_is_set(UCSR0A, RXC0);    // Wait for incoming data
     return UDR0;                            // read one byte from UART0
 }
