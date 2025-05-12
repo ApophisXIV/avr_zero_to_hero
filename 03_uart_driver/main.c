f94b7dd672605762f93717c944097ec571e11488
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2025-05-12 04:39:29 -0300

    [FIX] Conforms to GPIO driver naming convention

--- 03_uart_driver/main.c
+++ 03_uart_driver/main.c
@@ -2,10 +2,11 @@
 #include "Drivers/gpio/gpio.h"
 #include "Drivers/uart/uart.h"
 #include "board.h"
+extern void GPIO_EXTI_callback(GPIO_port_t port, GPIO_pin_t pin, GPIO_pin_state_t state);
 
 int main(void) {
 
-    gpio_config(GPIO_PORTB, GPIO_5, GPIO_OUTPUT_INITIAL_LOW);
+    GPIO_config(GPIO_PORTB, GPIO_5, GPIO_OUTPUT_INITIAL_LOW);
 
     UART_init();
 
@@ -16,10 +17,10 @@ int main(void) {
         // Get user input
         char input = getchar();
         if (input == '1') {
-            gpio_write_pin(GPIO_PORTB, GPIO_5, GPIO_HIGH);
+            GPIO_write_pin(GPIO_PORTB, GPIO_5, GPIO_HIGH);
             printf("LED ON\n");
         } else if (input == '0') {
-            gpio_write_pin(GPIO_PORTB, GPIO_5, GPIO_LOW);
+            GPIO_write_pin(GPIO_PORTB, GPIO_5, GPIO_LOW);
             printf("LED OFF\n");
         } else {
             printf("Invalid input\n");
