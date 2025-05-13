377c79f49a00aa90391dbdb5202a3ca4938c31f7
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2025-05-13 03:19:37 -0300

    [FIX] Add weak attribute to callback

--- 04_adc_driver/callbacks.c
+++ 04_adc_driver/callbacks.c
@@ -14,29 +14,37 @@
 
 #include "board.h"
 
+#ifdef USE_GPIO
+#include "Drivers/gpio/gpio.h"
+
+__attribute__((weak)) void GPIO_EXTI_callback(GPIO_port_t port, GPIO_pin_t pin, GPIO_pin_state_t state) {
+}
+#endif
+
 #ifdef USE_UART
 #include "Drivers/uart/uart.h"
 
-extern void UART_tx_callback(UART_handle_t *huart) {
+__attribute__((weak)) void UART_tx_callback(UART_handle_t *huart) {
 }
 
-extern void UART_rx_callback(UART_handle_t *huart) {
+__attribute__((weak)) void UART_rx_callback(UART_handle_t *huart) {
 }
 #endif
 
 #ifdef USE_TIMER
 #include "Drivers/timer/timer.h"
 
-extern void TIM_period_elapsed_callback(TIM_handle_t *htim) {
+__attribute__((weak)) void TIM_period_elapsed_callback(TIM_handle_t *htim) {
 }
 
-extern void TIM_CTC_callback(TIM_handle_t *htim) {
+__attribute__((weak)) void TIM_CTC_callback(TIM_handle_t *htim) {
 }
 #endif
 
 #ifdef USE_ADC
 #include "Drivers/adc/adc.h"
 
-extern void ADC_EOC_callback(ADC_handle_t *hadc) {
+__attribute__((weak)) void ADC_EOC_callback(ADC_handle_t *hadc, uint16_t value) {
 }
+
 #endif
